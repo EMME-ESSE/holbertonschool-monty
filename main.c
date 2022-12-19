58be29b7133b128aaefe009614f6472386f65442
EMME-ESSE <matimancini@gmail.com>
2022-12-18 18:53:17 -0800

    Main file

--- main.c
+++ main.c
@@ -1,39 +1,51 @@
 #include "monty.h"
-stack_t **full_start;
 /**
- * main - the root of the project
- * @argc: how many arguments were passed to a program
- * @argv: arguments vector
+ * main - entry point.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: on scuccess, always EXIT_SUCCESS
+ * Return: Always 0 (Success).
  */
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
+  stack_t **stack = NULL;
+  char *line = NULL;
+  size_t len = 0;
+  unsigned int line_number = 0;
+  FILE *fp;
+  int op;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "%s\n", "USAGE: monty file");
-		exit(EXIT_FAILURE);
-	}
-	fp = fopen(argv[1], "r");
-	if (!fp)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	line = malloc(sizeof(char) * 3);
-	if (!line)
-	{
-		free(line);
-		return (0);
-	}
-	while ((getline(&line, &len, fp)) != -1)
-	{
-		printf("%s", line);
-	}
-	fclose(fp);
-	exit(EXIT_SUCCESS);
+  if (argc != 2)
+  {
+    fprintf(stderr, "USAGE: %s file\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  fp = fopen(argv[1], "r");
+  if (!fp)
+  {
+    fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+  while (getline(&line, &len, fp) != -1)
+  {
+    char *opcode;
+    line_number++;
+    opcode = strtok(line, " \t\n");
+    if (!opcode || opcode[0] == '#')
+      continue;
+    op = get_op(opcode);
+    if (op < 0 || !op[opcode])
+    {
+      fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+      free_stack(stack);
+      free(line);
+      fclose(fp);
+      return EXIT_FAILURE;
+    }
+    return (op[opcode]);
+  }
+  free_stack(stack);
+  free(line);
+  fclose(fp);
+  return EXIT_SUCCESS;
 }
