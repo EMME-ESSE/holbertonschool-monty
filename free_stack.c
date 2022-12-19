4b7589731be34054caa5b646f552fd4ab34f0ded
EMME-ESSE <matimancini@gmail.com>
2022-12-18 18:53:35 -0800

    .

---
+++ free_stack.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * frer_stack - free stack.
+ * @stack: stack_t
+ *
+ *
+ * Return: Always 0
+ */
+void free_stack(stack_t **stack)
+{
+        stack_t *node, *temp;
+        if (!stack || !*stack)
+                return;
+        node = *stack;
+        while (node)
+        {
+                temp = node->next;
+                free(node);
+                node = temp;
+        }
+        *stack = NULL;
+}
