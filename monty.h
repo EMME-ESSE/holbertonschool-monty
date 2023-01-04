b3836b5114de3fe9379eb57c9697f74508c64c74
EMME-ESSE <matimancini@gmail.com>
2023-01-04 00:55:59 -0800

    Prototypes file

--- monty.h
+++ monty.h
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stddef.h>
-
+#include <fcntl.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -38,6 +38,10 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void (*get_op(char *s))(stack_t **stack, unsigned int line_number);
+
+int check_number(char *str);
+void push_err(unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
@@ -45,6 +49,5 @@ void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t **stack);
-int get_op(const char *opcode);
+void free_stack(stack_t *stack);
 #endif
