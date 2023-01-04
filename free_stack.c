b9a57614618194980a0208ce2946e070f2a52c82
EMME-ESSE <matimancini@gmail.com>
2023-01-04 00:58:48 -0800

    Free

--- free_stack.c
+++ free_stack.c
@@ -6,17 +6,17 @@
  *
  * Return: Always 0
  */
-void free_stack(stack_t **stack)
+void free_stack(stack_t *stack)
 {
         stack_t *node, *temp;
-        if (!stack || !*stack)
+        if (!stack)
                 return;
-        node = *stack;
+        node = stack;
         while (node)
         {
                 temp = node->next;
                 free(node);
                 node = temp;
         }
-        *stack = NULL;
+        stack = NULL;
 }
