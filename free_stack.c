8f31ab7327cfacf6eae30423720febf049d1cafd
EMME-ESSE <matimancini@gmail.com>
2023-01-04 01:09:36 -0800

    Free

--- free_stack.c
+++ free_stack.c
@@ -1,22 +1,20 @@
 #include "monty.h"
 /**
- * frer_stack - free stack.
+ * free_stack - free stack.
  * @stack: stack_t
- *
- *
  * Return: Always 0
  */
 void free_stack(stack_t *stack)
 {
-        stack_t *node, *temp;
-        if (!stack)
-                return;
-        node = stack;
-        while (node)
-        {
-                temp = node->next;
-                free(node);
-                node = temp;
-        }
-        stack = NULL;
+	stack_t *node, *temp;
+	if (!stack)
+		return;
+	node = stack;
+	while (node)
+	{	
+		temp = node->next;
+		free(node);
+		node = temp;
+	}
+	stack = NULL;
 }
